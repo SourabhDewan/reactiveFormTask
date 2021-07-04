f5b5c1268a094c405dee8dd6d501d798dec0f8e8
Sourabh Dewan <dewan.sourabh28@gmail.com>
2021-07-04 14:19:38 +0530

    Yes no toggle added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form-task';
+  flag = 0;
+  form = new FormGroup({
+    yearOfExposure : new FormControl('', Validators.required),
+    lastUsedThem : new FormControl('', Validators.required)
+  })
+
+  toDisplay(){
+    this.flag = 1;
+  }
+
+  notToDisplay(){
+    this.flag = 0;
+  }
 }
