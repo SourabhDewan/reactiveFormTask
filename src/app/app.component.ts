55c0516607799622ecced4971dd18048b4e3adcc
Lagnesh Thakur <lagneshthakur@gmail.com>
2021-07-14 21:19:13 +0530

    adds POST call on form submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,40 +11,39 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   title = 'form-task';
   flag = 0;
-  companyDetails;
   form;
   constructor(private fb : FormBuilder, private http : HttpClient){
 
   }
   ngOnInit(){
-    this.companyDetails = this.fb.group({
-      company: ['', Validators.required],
-      job: ['', Validators.required],
-      startingDate: ['', Validators.required],
-      tilDate: ['', Validators.required]
-  })
    
   this.form = this.fb.group({
     firstYearExposure: ['', Validators.required],
-    lastYearUsed: ['', Validators.required],
-    moreWork: this.fb.array([
-      this.companyDetails
+    lastYearUsedFrom: ['', Validators.required],
+    WorkHistories: this.fb.array([
     ])
   })
-  console.log(this.moreWork.controls);
+  this.addWorkHistories();
+  console.log(this.WorkHistories.controls);
   }
   
   
-  get moreWork(){
-    return this.form.get('moreWork') as FormArray;
+  get WorkHistories(){
+    return this.form.get('WorkHistories') as FormArray;
   }
 
-  addMoreWork(){
-    this.moreWork.push(this.companyDetails);
+  addWorkHistories(){
+    const workHistories = this.fb.group({
+      CompanyName: ['', Validators.required],
+      JobTitle: ['', Validators.required],
+      FromDate: ['', Validators.required],
+      ToDate: ['', Validators.required]
+  })
+    this.WorkHistories.push(workHistories);
   }
 
-  removeMoreWork(work: number){
-    this.moreWork.removeAt(work);
+  removeWorkHistories(work: number){
+    this.WorkHistories.removeAt(work);
   }
   toDisplay(){
     this.flag = 1;
@@ -55,12 +54,25 @@ export class AppComponent implements OnInit {
   }
 
   submit(){
-    this.http.get('https://api-tracker.sw-gr.com/pack/api/applicant/53?$expand=OccupationalHistories&$expand')
-    .subscribe(response => {
-      this.form.value = response;
-      console.log(response);
+    // this.http.get('https://api-tracker.sw-gr.com/pack/api/applicant/53?$expand=OccupationalHistories&$expand')
+    // .subscribe(response => {
+    //   this.form.value = response;
+    //   console.log(response);
+    // })
+    let formValue = this.form.value;
+    formValue.occupationalhistory = {
+      HasDiagnosedWithVwf: false,
+      HaveEverUsedHhvTools: false,
+      HhvFirstYearExposure: "2020-06-15T18:30:00.000Z",
+      LastTimeUsedHhv: "2021-06-13T18:30:00.000Z",
+      VwfDiagnosedStage: null,
+      packId: 1
+    }
+    console.log(formValue);
+
+    this.http.post("https://api-tracker.sw-gr.com/pack/api/Applicant/PostHVSdata?formId=7ced7b41-67a5-41a2-9aa3-42ca105e129d", formValue).subscribe(response => {
+
     })
-    console.log(this.form.value);
   }
 }
 
